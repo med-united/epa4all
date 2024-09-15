07a2d941b5e4d602eb57f6e616fe1a157e741f52
Alex Kontcur <alexander.kontsur@gmail.com>
2024-09-15 11:59:44 +0400

    EPA-275 Create custom CFX VAU Transport

--- information-service/src/test/java/de/servicehealth/epa4all/InformationServiceIT.java
+++ information-service/src/test/java/de/servicehealth/epa4all/InformationServiceIT.java
@@ -7,6 +7,9 @@ import io.quarkus.test.junit.TestProfile;
 import jakarta.inject.Inject;
 import org.apache.cxf.common.classloader.ClassLoaderUtils;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
+import org.apache.cxf.configuration.security.CertificateConstraintsType;
+import org.apache.cxf.ext.logging.LoggingInInterceptor;
+import org.apache.cxf.ext.logging.LoggingOutInterceptor;
 import org.apache.cxf.jaxrs.client.Client;
 import org.apache.cxf.jaxrs.client.ClientConfiguration;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
@@ -19,13 +22,18 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.KeyStore;
 import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -57,14 +65,22 @@ public class InformationServiceIT {
             Client client = WebClient.client(api);
             ClientConfiguration config = WebClient.getConfig(client);
             HTTPConduit conduit = (HTTPConduit) config.getConduit();
-            TLSClientParameters tlsParams = new TLSClientParameters();
+            conduit.getClient().setVersion("1.1");
+            
+            TLSClientParameters tlsParams = conduit.getTlsClientParameters();
+            if (tlsParams == null) {
+                tlsParams = new TLSClientParameters();
+                conduit.setTlsClientParameters(tlsParams);
+            }
+
+            tlsParams.setCertAlias("root");
             tlsParams.setSslContext(createSSLContext());
             tlsParams.setDisableCNCheck(true);
-            conduit.setTlsClientParameters(tlsParams);
+            // tlsParams.setUseHttpsURLConnectionDefaultSslSocketFactory(true);
+            tlsParams.setHostnameVerifier((hostname, session) -> true);
 
-            HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
-            httpClientPolicy.setVersion("1.1");
-            conduit.setClient(httpClientPolicy);
+            config.getOutInterceptors().add(new LoggingOutInterceptor());
+            config.getInInterceptors().add(new LoggingInInterceptor());
 
             assertDoesNotThrow(() -> api.getRecordStatus("Z1234567890", "PSSIM123456789012345/1.2.4"));
         } else {
@@ -73,21 +89,40 @@ public class InformationServiceIT {
     }
 
     private static SSLContext createSSLContext() throws Exception {
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-
-        KeyStore ts = KeyStore.getInstance("JKS");
-
-        // cfx.jks made with:
-        // keytool -importkeystore -deststorepass changeit -destkeystore cfx2.jks -srckeystore root.p12 -srcstoretype PKCS12
-        try (InputStream trustStore = ClassLoaderUtils.getResourceAsStream("cfx.jks", InformationServiceIT.class)) {
-            ts.load(trustStore, "changeit".toCharArray());
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        // String path = "/Users/bona/.sdkman/candidates/java/21.0.2-zulu/zulu-21.jdk/Contents/Home/lib/security/cacerts";
+        String path = "src/main/resources/keystore.jks";
+        try (InputStream trustStoreStream = new FileInputStream(path)) {
+            trustStore.load(trustStoreStream, "changeit".toCharArray());
         }
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        tmf.init(ts);
 
-        sslContext.init(null, tmf.getTrustManagers(), new SecureRandom());
+        System.setProperty("javax.net.ssl.trustStore", "/Users/bona/Work/ere.health/epa4all/information-service/src/main/resources/keystore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(trustStore);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustManagerFactory.getTrustManagers(), new SecureRandom());
         return sslContext;
     }
+
+    // private static SSLContext createSSLContext() throws Exception {
+    //     SSLContext sslContext = SSLContext.getInstance("TLS");
+    //
+    //     sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+    //         public X509Certificate[] getAcceptedIssuers() {
+    //             return new X509Certificate[0];
+    //         }
+    //
+    //         public void checkClientTrusted(X509Certificate[] certs, String authType) {
+    //         }
+    //
+    //         public void checkServerTrusted(X509Certificate[] certs, String authType) {
+    //         }
+    //     }}, new SecureRandom());
+    //     return sslContext;
+    // }
 
     public boolean informationServiceRunning() throws Exception {
         String containerName = "information-service";
