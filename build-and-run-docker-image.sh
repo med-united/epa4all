e347f4950fe10be03b2c425fff2cb708874ff3ad
Alex Kontsur <alexander.kontsur@gmail.com>
2025-01-13 19:53:24 +0400

    EPA-373

--- build-and-run-docker-image.sh
+++ build-and-run-docker-image.sh
@@ -7,6 +7,6 @@ docker build --progress=plain --no-cache -t epa4all .
 
 docker run --network host -d --name epa4all \
   -p 8090:8090 \
-  -v /Users/bona/Work/ere.health/incentergy.p12:/opt/epa4all/incentergy.p12 epa4all
+  -v /Users/bona/Work/ere.health/secret:/opt/epa4all/secret epa4all
 
 echo "docker image epa4all running. \n Status with: 'docker ps' \n Logs with: 'docker logs epa4all' \n Kill with: 'docker kill epa4all'"
--- linux-service/run.sh
+++ linux-service/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export QUARKUS_CONFIG_LOCATIONS=config
-export QUARKUS_PROFILE=RU
+#export QUARKUS_PROFILE=RU
 /usr/bin/java -jar quarkus-run.jar
