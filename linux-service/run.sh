01da77dc5721660f5975e25ad2afa580dc3973b1
Alex Kontsur <alexander.kontsur@gmail.com>
2025-01-28 10:39:02 +0400

    EPA-360 * jdwp support * volume creation in Dockerfile is removed

--- linux-service/run.sh
+++ linux-service/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export QUARKUS_CONFIG_LOCATIONS=config/application.properties
-/usr/bin/java -Dquarkus.profile=${QUARKUS_PROFILE:-mTLS-docker} -jar quarkus-run.jar
+/usr/bin/java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -Dquarkus.profile=${QUARKUS_PROFILE:-mTLS-docker} -jar quarkus-run.jar
