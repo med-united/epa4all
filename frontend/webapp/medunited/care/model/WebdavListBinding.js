0667a8fc81ef3a274c0d602b4b98258c6f2de714
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2025-01-28 23:39:18 +0000

    EPA-389 update WebdavListBinding.js

--- frontend/webapp/medunited/care/model/WebdavListBinding.js
+++ frontend/webapp/medunited/care/model/WebdavListBinding.js
@@ -12,6 +12,10 @@ sap.ui.define([
             this.aSorters = aSorters;
             this.aFilters = aFilters;
             this.mParameters = mParameters || {};
+
+            this.iStartIndex = 0;
+            this.iPageSize = mParameters && mParameters.pageSize ? mParameters.pageSize : 15;
+            this.bHasMoreData = true;
         },
 
         /**
@@ -20,24 +24,59 @@ sap.ui.define([
          * @param {int} iStartIndex The start index
          * @param {int} iLength The amount of data to be requested
          */
-        loadData: function (iStartIndex, iLength) {
+        loadData: function() {
+            const iStartIndex = this.iStartIndex;
+            const iPageSize = this.iPageSize;
+
             var that = this;
 
-            this.oModel.readWebdavFolder(this.sPath, iStartIndex, iLength)
-                .then(function (oData) {
-                    var existingData = that.oModel.getProperty(that.sPath) || [];
-                    var newData = Array.from(oData.getElementsByTagName("response"));
+            this.oModel.readWebdavFolder(this.sPath, iStartIndex, iPageSize)
+                .then(function(oData) {
+                    const aNewData = Array.from(oData.getElementsByTagName("response"));
+                    const iTotalItems = parseInt(oData.getElementsByTagName("totalItems")[0]?.textContent || "0", 10);
 
-                    existingData = existingData.concat(newData);
+                    var oDataNode = that.oModel.getObject(that.sPath);
+                    if (oDataNode) {
+                        aNewData.forEach((node) => oDataNode.appendChild(node.cloneNode(true)));
+                    }
 
-                    that.oModel.setProperty(that.sPath, existingData);
+                    const iTotalPages = Math.ceil(iTotalItems / iPageSize);
+                    that.bHasMoreData = iStartIndex + iPageSize < iTotalItems;
+
+                    that.oModel.refresh(true);
                     that.refresh();
+
                 })
-                .catch(function (oError) {
+                .catch(function(oError) {
                     console.error("Error loading data from WebDAV folder:", oError);
                 });
-        };
+        },
 
+        nextPage: function() {
+            if (this.bHasMoreData) {
+                this.iStartIndex += this.iPageSize;
+                this.loadData();
+            } else {
+                console.warn("No more data to load.");
+            }
+        },
+        previousPage: function() {
+            if (this.iStartIndex > 0) {
+                this.iStartIndex = Math.max(0, this.iStartIndex - this.iPageSize);
+                this.loadData();
+            } else {
+                console.warn("Already at the first page.");
+            }
+        },
+        setPage: function(iPage) {
+            const iNewStartIndex = (iPage - 1) * this.iPageSize;
+            if (iNewStartIndex >= 0) {
+                this.iStartIndex = iNewStartIndex;
+                this.loadData();
+            } else {
+                console.error("Invalid page number:", iPage);
+            }
+        }
     });
 
     return WebdavListBinding;
