da2834bc46130340cf38cc7f28c8510cfd0a0ba7
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2025-02-17 23:00:10 +0000

    CEIH-22 make documents section collapsible and hidden by default

--- frontend/webapp/medunited/care/SharedBlocks/document/DocumentBlock.js
+++ frontend/webapp/medunited/care/SharedBlocks/document/DocumentBlock.js
@@ -1,4 +1,17 @@
 sap.ui.define(['sap/uxap/BlockBase'], function (BlockBase) {
     "use strict";
-    return BlockBase.extend("medunited.care.SharedBlocks.document.DocumentBlock", {});
+    return BlockBase.extend("medunited.care.SharedBlocks.document.DocumentBlock", {
+        metadata: {
+            views: {
+                Collapsed: {
+                    viewName: "medunited.care.SharedBlocks.document.DocumentBlockCollapsed",
+                    type: "XML"
+                },
+                Expanded: {
+                    viewName: "medunited.care.SharedBlocks.document.DocumentBlockExpanded",
+                    type: "XML"
+                }
+            }
+        }
+    });
 }, true);
