6120f62f3e2889d6858e1bbd7b1bf36d44dc4df2
Manuel Blechschmidt <manuel.blechschmidt@service-health.de>
2025-03-17 11:12:45 +0100

    EPA-446 use base64 encoding not base64 url encoding

--- rest-server/src/main/java/de/servicehealth/epa4all/server/entitlement/EntitlementService.java
+++ rest-server/src/main/java/de/servicehealth/epa4all/server/entitlement/EntitlementService.java
@@ -133,6 +133,6 @@ public class EntitlementService {
         byte[] first5 = new byte[5];
         System.arraycopy(sha256, 0, first5, 0, 5);
         first5[0] = (byte) (first5[0] & 127);
-        return Base64.getUrlEncoder().encodeToString(first5);
+        return Base64.getEncoder().encodeToString(first5);
     }
 }
