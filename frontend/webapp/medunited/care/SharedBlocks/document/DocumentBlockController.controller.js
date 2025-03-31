a938fc603bdd57a72c50ea87b13ad3e0e8a36ddd
Beatriz Correia <beatriz.correia.pt@gmail.com>
2025-03-31 07:55:21 +0100

    EPA-447 add fulltext search inside patient details (#94)
    * initial version with Dokumente tab
    
    * add fulltext search inside patient details

--- frontend/webapp/medunited/care/SharedBlocks/document/DocumentBlockController.controller.js
+++ frontend/webapp/medunited/care/SharedBlocks/document/DocumentBlockController.controller.js
@@ -8,8 +8,9 @@ sap.ui.define([
     "sap/m/MessageToast",
     "sap/m/MessageBox",
     "sap/m/ColumnListItem",
-	"sap/ui/model/ChangeReason"
-], function (AbstractController, Formatter, FHIRFilter, FHIRFilterType, FHIRFilterOperator, Item, MessageToast, MessageBox, ColumnListItem, ChangeReason) {
+	"sap/ui/model/ChangeReason",
+	'sap/ui/model/json/JSONModel',
+], function (AbstractController, Formatter, FHIRFilter, FHIRFilterType, FHIRFilterOperator, Item, MessageToast, MessageBox, ColumnListItem, ChangeReason, JSONModel) {
     "use strict";
 
     return AbstractController.extend("medunited.care.SharedBlocks.document.DocumentBlockController", {
@@ -19,6 +20,8 @@ sap.ui.define([
         onInit: function () {
             this.initializeRouter();
 
+            var oModel = new JSONModel({ results: [] });
+            this.getView().setModel(oModel, "documents");
         },
 
         initializeRouter: function () {
@@ -36,6 +39,29 @@ sap.ui.define([
 				"document": encodeURIComponent(oEvent.getSource().getBindingContext().getProperty("href"))
 			});
 		},
+		onSearchDocumentItemPress: function (oEvent) {
+            const oItemContext = oEvent.getSource().getBindingContext("documents");
+            const sWebdavPath = oItemContext.getProperty("href");
+            const sMimeType = oItemContext.getProperty("mimeType");
+
+            const oContext = this.getView().getBindingContext();
+            const sPatientPath = oContext?.getPath();
+            const sLastId = sPatientPath?.split(/\//).pop();
+
+            console.log("Opening document:", sWebdavPath);
+
+            if (sWebdavPath && sLastId) {
+                const sWebdavPath = oItemContext.getProperty("href");
+
+                this.oRouter.navTo("patient-detail", {
+                  patient: sLastId,
+                  layout: "ThreeColumnsEndExpanded",
+                  document: encodeURIComponent(sWebdavPath)
+                });
+            } else {
+                MessageToast.show("Document URL or patient ID missing");
+            }
+        },
         onPatientRouteMatched: function (oEvent) {
             this._entity = oEvent.getParameter("arguments").patient;
         },
@@ -47,6 +73,103 @@ sap.ui.define([
         },
         onSuggestDocumentName: function (oEvent) {
             
+        },
+        onSearchDocuments: function (oEvent) {
+            const sQuery = oEvent.getParameter("query");
+
+            const oContext = this.getView().getBindingContext();
+            const sPatientId = oContext?.getProperty("propstat/prop/displayname");
+
+            const oTable = this.byId("documentTable");
+
+            let oDocumentsModel = this.getView().getModel("documents");
+            if (!oDocumentsModel) {
+                oDocumentsModel = new sap.ui.model.json.JSONModel({ results: [] });
+                this.getView().setModel(oDocumentsModel, "documents");
+            }
+
+            if (!sQuery) {
+                oDocumentsModel.setProperty("/results", []);
+                if (oTable) oTable.setVisible(false);
+                return;
+            }
+
+            if (!sPatientId) {
+                MessageToast.show("Patient ID missing");
+                return;
+            }
+
+            sap.ui.core.BusyIndicator.show(0);
+
+            const sSearchXML = `<d:searchrequest xmlns:d="DAV:">
+                <d:JCR-SQL2><![CDATA[
+                    SELECT * FROM [nt:resource] AS r
+                    WHERE CONTAINS(r.*, '${sQuery}')
+                ]]></d:JCR-SQL2>
+            </d:searchrequest>`;
+
+            const sTelematikId = localStorage.getItem("telematikId");
+            if (!sTelematikId) {
+                sap.ui.core.BusyIndicator.hide();
+                MessageToast.show("Telematik ID is missing");
+                return;
+            }
+
+            const sUrl = `/webdav2/${sTelematikId}/jcr:root/rootFolder`;
+            const that = this;
+
+            $.ajax({
+                url: sUrl,
+                method: "SEARCH",
+                data: sSearchXML,
+                contentType: "text/xml",
+                processData: false,
+                success: function (data, textStatus, jqXHR) {
+                    sap.ui.core.BusyIndicator.hide();
+
+                    const xmlDoc = jqXHR.responseXML;
+                    if (!xmlDoc) {
+                        oDocumentsModel.setProperty("/results", []);
+                        if (oTable) oTable.setVisible(false);
+                        return;
+                    }
+
+                    const aResults = [];
+                    $(xmlDoc).find("D\\:response, response").each(function () {
+                        const sHref = $(this).find("D\\:href, href").text();
+                        const sPath = $(this).find("dcr\\:column dcr\\:name:contains('jcr:path')").siblings("dcr\\:value").text();
+                        const sEncoding = $(this).find("dcr\\:column dcr\\:name:contains('r.jcr:encoding')").siblings("dcr\\:value").text();
+                        const sLastModified = $(this).find("dcr\\:column dcr\\:name:contains('r.jcr:lastModified')").siblings("dcr\\:value").text();
+                        const sMimeType = $(this).find("dcr\\:column dcr\\:name:contains('r.jcr:mimeType')").siblings("dcr\\:value").text();
+
+                        const sDecodedHref = decodeURIComponent(sHref);
+
+                        const sWebdavPath = decodeURIComponent(sHref)
+                          .replace(/^.*\/webdav2\//, "/webdav/")
+                          .replace(/jcr:root\/rootFolder\//, "")
+                          .replace(/\/jcr:content\/?$/, "");
+
+                        aResults.push({
+                          path: sPath,
+                          href: sWebdavPath,
+                          encoding: sEncoding,
+                          lastModified: sLastModified,
+                          mimeType: sMimeType
+                        });
+                    });
+
+                    const aFiltered = aResults.filter(doc => doc.path.includes(`/${sPatientId}/`));
+                    oDocumentsModel.setProperty("/results", aFiltered);
+                    if (oTable) oTable.setVisible(aFiltered.length > 0);
+                },
+                error: function (oError) {
+                    sap.ui.core.BusyIndicator.hide();
+                    console.error("SEARCH request failed:", oError);
+                    MessageToast.show("Search failed. Please check server logs.");
+                    oDocumentsModel.setProperty("/results", []);
+                    if (oTable) oTable.setVisible(false);
+                }
+            });
         }
     });
 });
