daaf1784676fb8c76963033cecc0acb3a4421d37
alexanderremmes <a.remmes@icloud.com>
2026-04-27 12:33:30 +0200

    Adding REF certs to import script

--- tls/epa-certs/import-certs.sh
+++ tls/epa-certs/import-certs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo keytool -importcert -file epa-as-1.dev.epa4all.de.pem -alias epa-as-1.dev.epa4all.de -cacerts
 sudo keytool -importcert -file epa-as-2.dev.epa4all.de.pem -alias epa-as-2.dev.epa4all.de -cacerts
+sudo keytool -importcert -file epa-as-1-ref.epa4all.de.pem -alias epa-as-1-ref.epa4all.de -cacerts
+sudo keytool -importcert -file epa-as-2-ref.epa4all.de.pem -alias epa-as-2-ref.epa4all.de -cacerts
